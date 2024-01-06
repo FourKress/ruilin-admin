e83837ce1f5227b7073a7c20d1b9f121fe88fea7
FourKress <511970747@qq.com>
2024-01-06 23:27:44 +0800

    feat: init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,11 +8,47 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'simple-import-sort', 'import', 'unused-imports'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // react放在首行
+          ['^react', '^@?\\w'],
+          // 内部导入
+          ['^(@|components)(/.*|$)'],
+          // 父级导入. 把 `..` 放在最后.
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // 同级导入. 把同一个文件夹.放在最后
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          // 图片资源导入.
+          ['^.+\\.?(svg|png|jpg)$'],
+          // 样式导入.
+          ['^.+\\.?(css)$']
+        ]
+      }
+    ],
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_'
+      }
+    ]
   },
 }
