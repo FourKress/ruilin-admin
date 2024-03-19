53a400f7747bb51eecaccd366ca686495266d02f
FourKress <511970747@qq.com>
2024-03-19 20:36:27 +0800

    feat: init

--- server.sh
+++ server.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
-echo '---开始执行git checkout main---'
-git checkout main
+branch=""
+env=""
+
+if [ "$1" == "test" ]
+then
+  branch="test"
+  env="test"
+elif [ "$1" == "prod" ]
+then
+  branch="main"
+  env="production"
+else
+  echo '---参数错误，执行失败---'
+  exit 1
+fi
+
+echo $env
+
+echo "---开始执行git checkout $branch---"
+git checkout $branch
 echo '---开始执行git pull---'
 git pull
 echo '---git pull执行完毕，pnpm install---'
 pnpm i
 echo '---pnpm install执行完毕，开始执行pnpm build---'
 pnpm build
+export NODE_ENV=$env
 echo '---pnpm build执行完毕 服务启动成功---'
