17cda5932e2bfcc640023ac1ecdaec6693e4db5f
FourKress <511970747@qq.com>
2024-03-19 21:10:19 +0800

    feat: init

--- server.sh
+++ server.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
 branch=""
-env=""
 
 if [ "$1" == "test" ]
 then
   branch="test"
-  env="test"
 elif [ "$1" == "prod" ]
 then
   branch="main"
-  env="production"
 else
   echo '---参数错误，执行失败---'
   exit 1
 fi
-
-echo $env
 
 echo "---开始执行git checkout $branch---"
 git checkout $branch
@@ -25,5 +20,5 @@ git pull
 echo '---git pull执行完毕，pnpm install---'
 pnpm i
 echo '---pnpm install执行完毕，开始执行pnpm build---'
-tsc && vite build --mode $env
+pnpm build:$1
 echo '---pnpm build执行完毕 服务启动成功---'
